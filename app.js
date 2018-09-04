7b57ed31a52ef1da781c0efff95f848278b06a43
MRobinson <mrobinson928@hotmail.com>
2018-09-04 10:34:40 -0400

    Updated JS

--- app.js
+++ app.js
@@ -49,24 +49,67 @@ const command = prompt('Enter a Command: print, verify, lookup, contains, update
 const msg = 'Not A Valid Command. Try Again.'
 
 for ( let i = 0; i < commandList.length; i++){
-  if (command === 'print'){
+  if (command.toLocaleLowerCase() === 'print'){
     render(employeeList[i].name);
     render(employeeList[i].officeNum);
     render(employeeList[i].phoneNum);
   }
-  else if(command === 'verify'){
-  for (let i = 0; i < employeeList.length; i++){
+  else if(command.toLocaleLowerCase() === 'verify'){
     const employee = prompt('Enter Employee Name')
-     if (employee === employeeList[i])
-      render('true')
+    let verify = 'false'
+  for (let i = 0; i < employeeList.length; i++){
+     if (employee === employeeList[i].name){
+      verify = 'true';
+      render(verify)
+      }
+     }
     }
-  }
-}
-render(msg)
+    else if(command.toLocaleLowerCase() === 'lookup'){
+      const employee = prompt('Enter Employee Name')
+    for (let i = 0; i < employeeList.length; i++){
+       if (employee === employeeList[i].name){
+        render(employeeList[i].name);
+        render(employeeList[i].officeNum);
+        render(employeeList[i].phoneNum);
+        }
+       }
+      }
+      else if(command.toLocaleLowerCase() === 'contains'){
+        const employee = prompt('Enter Employee Name')
+      for (let i = 0; i < employeeList.length; i++){
+         if (employeeList[i].includes(element, start) === employee){
+          render(employeeList[i].name);
+          }
+         }
+        }
+        else if(command.toLocaleLowerCase() === 'update'){
+          const employee = prompt('Enter Employee Name')
+        for (let i = 0; i < employeeList.length; i++){
+           if (employee === employeeList[i].name){
+            const field = prompt('What would you like to change?')
+            }
+           }
+          }
 
-const employee = prompt('Enter Employee Name')
-for ( let i = 0; i < commandList.length; i++){
-    if(command === 'verify')
-     if (employee === employeeList[i])
-      render('true')
-}
+          else if(command.toLocaleLowerCase() === 'add'){
+              const employeeName = prompt('Enter Employee Name?');
+              const officeNum = prompt('Enter Office Number');
+              const phoneNum = prompt('Enter Telephone Number');
+             var newEmployee = {
+                name: employeeName,
+                officeNum: officeNum,
+                phoneNum: phoneNum,
+             }
+              
+            
+              employeeList.push(newEmployee);
+            
+              for (let i = 0; i < employeeList.length; i++) {
+                render(employeeList[i].name);
+                render(employeeList[i].officeNum);
+                render(employeeList[i].phoneNum);
+              }
+          } 
+              
+  }
+render(msg);
